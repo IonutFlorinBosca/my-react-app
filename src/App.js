b26a460f753986d013d7c818e45d62995d618f87
Ionut <ionutflorinbosca@yahoo.com>
2022-05-12 07:26:26 +0200

    CHANGES

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        My life is a mess but I will turn it around!
+        FIRST CHANGES
       </header>
     </div>
   );
